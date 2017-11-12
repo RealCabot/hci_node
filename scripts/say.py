392eb77d295806adb2ba4c466fabacdf1f900131
andrewww2 <andrwww@utexas.edu>
2017-11-12 12:39:54 -0500

    Added callback for subscriber in say.py
    We need to launch a soundplay node, wait 1 sec, then issue speech commands. There should be a more efficient way to do this.

--- scripts/say.py
+++ scripts/say.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 import rospy
 from sound_play.libsoundplay import SoundClient
+from std_msgs.msg import String
 
-#self.soundhandle = SoundClient()
-soundhandle = SoundClient()
-rospy.init_node('say')
-rospy.sleep(1)
-#self.soundhandle.say('Take me to your leader.')
-soundhandle.say('YEAAAA BOYYYY')
+soundhandle = None
+
+def speak(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    soundhandle.say(data.data)
+
+
+def say_something():
+    soundhandle = SoundClient()
+    rospy.init_node('say', anonymous=True)
+    rospy.Subscriber("chatter", String, speak)
+    rospy.sleep(1) #give time for soundplay node to initialize
+    soundhandle.say('Is this thing on?')
+    # spin() simply keeps python from exiting until this node is stopped
+    #rospy.spin()
+
+if __name__ == '__main__':
+    say_something()
+
+
