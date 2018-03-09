c262e799b30936188dd3790d04bb96616cc43c9c
yanda Huang <ward11sdo@qq.com>
2018-03-09 14:21:36 -0500

    Try to make the thing into a class and tell ped number

--- scripts/ped_say.py
+++ scripts/ped_say.py
@@ -1,42 +1,27 @@
 #!/usr/bin/env python
 import rospy
 from sound_play.libsoundplay import SoundClient
-import numpy as np
-
-from geometry_msgs import *
-
-from math import *
 from kinect2_tracker.msg import user_points
 
 soundhandle = None
 warnThreshold = None  # distance to point (in meters) before we warn the user
 
-def getPed(msg):
-    global soundhandle, warnThreshold
+class Pedestrian_Warner:
+    def __init__(self):
+        self.soundhandle = SoundClient()
+        self.warnThreshold = 1.2 # in meters
+        rospy.init_node('pedestrian_warner', anonymous=True)
+        rospy.Subscriber("/people_points", user_points, self.getPed)
+        rospy.sleep(1)  # give time for soundplay node to initialize
+        soundhandle.say('Its your boi') #confirm that soundplay is working
+        rospy.spin()
 
-    peds = msg.people_points
-    if len(peds) > 0: #number of pedestrians
+    def getPed(self, msg):
 
-        for p in peds:
-            newPt = p.point.z #z is depth??
-            if newPt < warnThreshold:
-
-                warnMsg = "Pedestrains ahead"
-                print warnMsg
-                soundhandle.say(warnMsg)  # speak warning to user
-
-
-# Code based on: https://answers.ros.org/question/10829/text-to-speech-in-a-python-node/
-def init():
-    global soundhandle, warnThreshold
-    warnThreshold= 1.2 #in meters
-    soundhandle = SoundClient()
-    rospy.init_node('say', anonymous=True)
-    rospy.Subscriber("people_points", user_points, getPed)
-    rospy.sleep(1)  # give time for soundplay node to initialize
-    soundhandle.say('Its your boi') #confirm that soundplay is working
-    rospy.spin()
-
+        peds = msg.people_points
+        if len(peds) > 0:
+            warned_number = len([p for p in peds if p.point.z < warnThreshold])
+            self.soundhandle.say('{} pedestrains ahead'.format(warned_number))
 
 if __name__ == '__main__':
-    init()
+    warner = Pedestrian_Warner()
