e6076830bc8c75c4eaa96a69e5014b57d2e7a084
yanda Huang <ward11sdo@qq.com>
2018-03-09 14:53:31 -0500

    Add in pedestrain number, refactor to class

--- scripts/ped_say.py
+++ scripts/ped_say.py
@@ -3,25 +3,24 @@ import rospy
 from sound_play.libsoundplay import SoundClient
 from kinect2_tracker.msg import user_points
 
-soundhandle = None
-warnThreshold = None  # distance to point (in meters) before we warn the user
-
 class Pedestrian_Warner:
     def __init__(self):
         self.soundhandle = SoundClient()
         self.warnThreshold = 1.2 # in meters
+        self.prev_peds = 0
         rospy.init_node('pedestrian_warner', anonymous=True)
         rospy.Subscriber("/people_points", user_points, self.getPed)
         rospy.sleep(1)  # give time for soundplay node to initialize
-        soundhandle.say('Its your boi') #confirm that soundplay is working
+        self.soundhandle.say('Its your boi') #confirm that soundplay is working
         rospy.spin()
 
     def getPed(self, msg):
-
         peds = msg.people_points
         if len(peds) > 0:
-            warned_number = len([p for p in peds if p.point.z < warnThreshold])
-            self.soundhandle.say('{} pedestrains ahead'.format(warned_number))
+            curr_peds = len([p for p in peds if p.point.z < self.warnThreshold])
+            if self.prev_peds != curr_peds:
+                self.soundhandle.say('{num} pedestrains ahead'.format(num=curr_peds))
+            self.prev_peds = curr_peds
 
 if __name__ == '__main__':
     warner = Pedestrian_Warner()
