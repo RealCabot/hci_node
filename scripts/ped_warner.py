c20e48b6246467453b09feeb6e1925e5a8c3d283
andrewww2 <awong2@andrew.cmu.edu>
2018-03-11 00:24:17 -0500

    Ped_warner needs more testing
    It seems that the corner_extractor only runs after 2D nav goal is set for a 2nd time

--- scripts/ped_warner.py
+++ scripts/ped_warner.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import rospy
+from scipy.spatial import distance
 from sound_play.libsoundplay import SoundClient
 from kinect2_tracker.msg import user_points
+import numpy as np
+from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import PoseWithCovarianceStamped
 import dynamic_reconfigure.client
 
 class Pedestrian_Warner:
@@ -10,18 +14,20 @@ class Pedestrian_Warner:
         self.soundhandle = SoundClient()
         self.warnThreshold = 1.2 # in meters
         self.prev_peds = 0
-        
+
+        self.cornerPts = None
         rospy.init_node('pedestrian_warner', anonymous=True)
-        
-        rospy.Subscriber("/people_points", user_points, self.getPed)
+        rospy.Subscriber("/corners", PoseArray, self.get_corners)
+        rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.warn_corner)
+        rospy.Subscriber("/people_points", user_points, self.get_ped)
         self.config_client = dynamic_reconfigure.client.Client("/move_base/DWAPlannerROS", timeout=5)
         self.max_speed = self.config_client.get_configuration()['max_vel_x']
 
         rospy.sleep(1)  # give time for soundplay node to initialize
-        self.soundhandle.say('Its your boi') #confirm that soundplay is working
+        self.soundhandle.say('Its your boi') # confirm that soundplay is working
         rospy.spin()
 
-    def getPed(self, msg):
+    def get_ped(self, msg):
         peds = msg.people_points
         curr_peds = len([p for p in peds if p.point.z < self.warnThreshold])
         if self.prev_peds != curr_peds:
@@ -34,6 +40,33 @@ class Pedestrian_Warner:
     
     def scared_speed(self, num_peds):
         return self.max_speed / (num_peds+1)
+
+
+    # called when the corner extractor publishes points
+    def get_corners(self, msg):
+        corners = msg.poses
+        # make a 3xN matrix, where N is number of corners, [x y z]^T
+        self.cornerPts = np.zeros((0, 3))
+        for p in corners:
+            coord = p.position
+            pt = np.array([[coord.x, coord.y, coord.z]])
+            self.cornerPts = np.vstack((self.cornerPts, pt))
+        print(self.cornerPts)
+
+
+    # warn the user if we are approaching a corner
+    def warn_corner(self, msg):
+        if self.cornerPts is not None and len(self.cornerPts) > 0:
+            # find min euclidean distance between current position and list of corner points
+            coord = msg.pose.pose.position
+            curr_pt = np.array([[coord.x, coord.y, coord.z]])
+            dist = distance.cdist(self.cornerPts, curr_pt, 'euclidean')
+            #print(self.cornerPts, curr_pt)
+            min_dist = np.amin(dist)
+            arg_min = np.argmin(dist)
+            print('ind: ',arg_min, 'dist:',min_dist)
+            if min_dist < self.warnThreshold:
+                self.soundhandle.say('Turn {dist} meters ahead'.format(dist=min_dist))
 
 
 if __name__ == '__main__':
