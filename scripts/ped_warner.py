df309e6e3fadf176342445dfd086d888c052c840
andrewww2 <awong2@andrew.cmu.edu>
2018-03-11 22:37:14 -0500

    Added left/right turn warning and call corner_extractor as function instead of its own node

--- scripts/ped_warner.py
+++ scripts/ped_warner.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 import rospy
-from scipy.spatial import distance
+#from scipy.spatial import distance
 from sound_play.libsoundplay import SoundClient
 from kinect2_tracker.msg import user_points
 import numpy as np
-from geometry_msgs.msg import PoseArray
+from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from corner_extract_standalone import Extractor
 import dynamic_reconfigure.client
 
 class Pedestrian_Warner:
     def __init__(self):
         
         self.soundhandle = SoundClient()
-        self.warnThreshold = 3  # in meters. Originally 1.2
+        self.corner_extractor = Extractor()
+        self.warnThreshold = 2  # in meters. Originally 1.2
         self.prev_peds = 0
 
         self.cornerPts = None
         rospy.init_node('pedestrian_warner', anonymous=True)
-        rospy.Subscriber("corners", PoseArray, self.get_corners)
+        rospy.Subscriber('/move_base/NavfnROS/plan', Path, self.get_corners)
+        #rospy.Subscriber("corners", PoseArray, self.get_corners)
         rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.warn_corner)
         rospy.Subscriber("/people_points", user_points, self.get_ped)
         self.config_client = dynamic_reconfigure.client.Client("/move_base/DWAPlannerROS", timeout=5)
@@ -43,32 +46,28 @@ class Pedestrian_Warner:
 
     # called when the corner extractor publishes points
     def get_corners(self, msg):
-        corners = msg.poses
-        # make a Nx3 matrix, where N is number of corners, [x y z]^T
-        self.cornerPts = np.zeros((0, 3))
-        for p in corners:
-            coord = p.position
-            pt = np.array([[coord.x, coord.y, coord.z]])
-            self.cornerPts = np.vstack((self.cornerPts, pt))
-        print('Received corners:\n', self.cornerPts)
+        self.cornerPts = self.corner_extractor.analyze_plan(msg)
 
     # warn the user if we are approaching a corner
     def warn_corner(self, msg):
         if self.cornerPts is not None and len(self.cornerPts) > 0:
-            # find min euclidean distance between current position and list of corner points
-            coord = msg.pose.pose.position
-            curr_pt = np.array([[coord.x, coord.y, coord.z]]) #CaBot's current position
-            dist = distance.cdist(self.cornerPts, curr_pt, 'euclidean') # find distance between curr_pt and each row
-            #print(self.cornerPts, curr_pt)
-            min_dist = np.amin(dist)
+            # find euclidean distances between current position and list of corner points
+            curr_pt = msg.pose.pose.position
+            dist = []
+            for c in self.cornerPts:
+                corn_pt = c.pose.position
+                d = np.sqrt((curr_pt.x-corn_pt.x)**2 + (curr_pt.y-corn_pt.y)**2)  # find euclidean distance
+                dist.append(d)
+
+            min_dist = min(dist)
             min_ind = np.argmin(dist)
-            print 'pt:', self.cornerPts[min_ind, :], 'ind:', min_ind, 'dist:', min_dist
 
             if min_dist < self.warnThreshold:
-                print '!_TURN AHEAD_!'
-                # delete closest point so we stop warning the user
-                self.cornerPts = np.delete(self.cornerPts, min_ind, 0)
-                self.soundhandle.say('Turn {dist} meters ahead'.format(dist=min_dist))
+                # print '!_TURN AHEAD_!'
+                turn_dir = self.cornerPts[min_ind].turnType
+                self.cornerPts.pop(min_ind)  # delete closest point so we stop warning the user
+                # format to only print 1 decimal place
+                self.soundhandle.say('Turn ' + turn_dir + ' in {dist} meters'.format(dist='%.1f' % min_dist))
 
 
 if __name__ == '__main__':
